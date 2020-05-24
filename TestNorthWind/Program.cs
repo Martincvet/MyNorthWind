0f2b95fb14de2962246a4dcac3bea49daae85338
Martin Cvet <martincvet@outlook.com>
2020-05-24 02:14:37 +0200

    Add project files.

---
+++ TestNorthWind/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace TestNorthWind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new NorthwindContext();
+            var employees = context.Employees.ToList().OrderBy(x=>x.FirstName);
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"Hello {employee.FirstName} {employee.LastName}!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
